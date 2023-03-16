fb43ae24607e250800f0c9c957acdacb307908fe
walshbr <bmw9t@virginia.edu>
2023-03-16 15:51:47 -0400

    pushing one last time

--- analysis.py
+++ analysis.py
@@ -4,6 +4,7 @@ import nltk
 filename = "antes.txt"
 with open(filename, 'r') as fin:
     raw_text = fin.read()
+
 # do something with it
 num_characters = len(raw_text)
 # split that long string on spaces
@@ -22,3 +23,11 @@ print("number of words split by nltk's tokenizer: " + str(len(raw_tokens)))
 print("num of unique words: " + str(len(unique_words)))
 
 nltk_text = nltk.Text(raw_tokens)
+
+def find_chiasmus(text):
+    pass
+
+# TODO: annotate the code in your own words
+# TODO: take an example of chiasmus and try to write out how you would define it. in computer terms
+# for example - end of sentence (or line). look x number of words before and after and see if they share a term in both places.
+# TODO: for next time, we will take one of those definitions and code it together as a function. 
